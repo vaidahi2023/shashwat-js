bfc715bd2e0db334b0277aec7d2132f41bde29b8
vaidahi2023 <vaidahi810@gmail.com>
2025-02-23 16:49:47 +0530

    introduction,operators

---
+++ datatype.js
@@ -0,0 +1,25 @@
+// var --global scope 
+// let --local scope
+
+//concat --join two string 
+var a=10
+var b="riya"
+var f=12.34
+var s='we'
+var bol=true
+const ch=20
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof f)
+console.log(typeof s)
+console.log(typeof bol)
+console.log(typeof ch)
+// ch=80
+let c=30
+//cout<<"   "<<a;
+// console.log("c="+c)
+// console.log(a+c)
+// console.log("sum="+a+c) 
+// console.log("sum="+(a+c)) 
+
